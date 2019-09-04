4d4b44fb326f34026272d68d582928f3749fe38a
Nikita Karpunin <nikita.karpunin@iss.digital>
2019-09-04 22:22:24 +0300

    Исправление вылета в разделе жизнь

--- app/src/main/java/com/mai/nix/maiapp/SportSectionsFragment.java
+++ app/src/main/java/com/mai/nix/maiapp/SportSectionsFragment.java
@@ -2,19 +2,22 @@ package com.mai.nix.maiapp;
 
 import android.os.AsyncTask;
 import android.widget.Toast;
+
 import com.mai.nix.maiapp.model.SportSectionsBodies;
 import com.mai.nix.maiapp.model.SportSectionsHeaders;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 
 /**
  * Created by Nix on 11.08.2017.
  */
 
-public class SportSectionsFragment extends SimpleExpandableListFragment{
+public class SportSectionsFragment extends SimpleExpandableListFragment {
 
     @Override
     protected void releaseThread() {
@@ -25,6 +28,7 @@ public class SportSectionsFragment extends SimpleExpandableListFragment{
         private Document doc;
         private Element table;
         private Elements rows, headers;
+
         public MyThread() {
             super();
         }
@@ -38,23 +42,29 @@ public class SportSectionsFragment extends SimpleExpandableListFragment{
                 rows = table.select("tr");
                 headers = table.select("th");
                 if (table != null) mHeaders.clear();
-                for(int i = 0; i < headers.size(); i++){
+                for (int i = 0; i < headers.size(); i++) {
                     mHeaders.add(new SportSectionsHeaders(headers.get(i).text()));
                 }
                 int j = 0;
-                for(int i = 1; i < rows.size(); i++){
+                for (int i = 1; i < rows.size(); i++) {
                     Elements el = rows.get(i).select("td");
-                    if(!el.isEmpty()){
-                        SportSectionsBodies body = new SportSectionsBodies(el.get(0).text(), el.get(1).text(), el.get(2).text());
-                        mHeaders.get(j).addBody(body);
-                    }else{
+                    if (!el.isEmpty()) {
+                        if (el.size() > 2) {
+                            SportSectionsBodies body = new SportSectionsBodies(el.get(0).text(), el.get(1).text(), el.get(2).text());
+                            mHeaders.get(j).addBody(body);
+                        } else {
+                            SportSectionsBodies body = new SportSectionsBodies(el.get(0).text(), "", el.get(1).text());
+                            mHeaders.get(j).addBody(body);
+                        }
+
+                    } else {
                         j++;
                     }
                 }
                 size = rows.size();
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
-            }catch (NullPointerException n){
+            } catch (NullPointerException n) {
                 return 0;
             }
             return size;
@@ -63,12 +73,12 @@ public class SportSectionsFragment extends SimpleExpandableListFragment{
         @Override
         protected void onPostExecute(Integer s) {
             mSwipeRefreshLayout.setRefreshing(false);
-            if(s == 0){
+            if (s == 0) {
                 if (getContext() != null) Toast.makeText(getContext(), R.string.error,
                         Toast.LENGTH_LONG).show();
-            }else{
+            } else {
                 mExpandableListView.setAdapter(mAdapter);
-                for(int i = 0; i < mHeaders.size(); i++){
+                for (int i = 0; i < mHeaders.size(); i++) {
                     mExpandableListView.expandGroup(i);
                 }
             }
