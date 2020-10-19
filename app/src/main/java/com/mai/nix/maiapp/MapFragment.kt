2e839feaa1a0324d1bba7a192071370fc3797aa5
Nikita Karpunin <nikita.karpunin@iss.digital>
2020-10-19 23:14:33 +0300

    Перевод фрагмента карты на котлин

---
+++ app/src/main/java/com/mai/nix/maiapp/MapFragment.kt
@@ -0,0 +1,32 @@
+package com.mai.nix.maiapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_map_layout.*
+
+/**
+ * Created by Nix on 13.05.2017.
+ */
+
+class MapFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_map_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        campusView.settings.setSupportZoom(true)
+        campusView.settings.builtInZoomControls = true
+        campusView.setPadding(0, 0, 0, 0)
+        campusView.isScrollbarFadingEnabled = true
+        campusView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        campusView.loadUrl("file:///android_asset/scheme_for_app.png")
+        legendButton.setOnClickListener {
+            val dialog = LegendDialog()
+            dialog.show(childFragmentManager, "LegendDialog")
+        }
+    }
+}
