77e86f5a9ae1734355f88abac508c1a6266757cb
Nikita Karpunin <nikita.karpunin@iss.digital>
2020-11-01 19:09:25 +0300

    Удалено лишнее

--- app/src/main/java/com/mai/nix/maiapp/navigation_fragments/settings/SettingsIntent.kt
+++
@@ -1,6 +0,0 @@
-package com.mai.nix.maiapp.navigation_fragments.settings
-
-sealed class SettingsIntent {
-    object ClearExamsCacheIntent: SettingsIntent()
-    object ClearSubjectsCacheIntent: SettingsIntent()
-}
--- app/src/main/java/com/mai/nix/maiapp/navigation_fragments/settings/SettingsState.kt
+++
@@ -1,7 +0,0 @@
-package com.mai.nix.maiapp.navigation_fragments.settings
-
-sealed class SettingsState {
-    object SettingsIdle: SettingsState()
-    data class ExamsCacheCleared(val success: Boolean, val error: String?): SettingsState()
-    data class SubjectsCacheCleared(val success: Boolean, val error: String?): SettingsState()
-}
